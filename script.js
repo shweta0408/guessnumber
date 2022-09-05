c201fac5d88d0490a919704f23792dd9e9cfe3e9
Shweta Mishra <shmis2004@gmail.com>
2022-09-05 22:27:37 +0530

    dom manipulation

---
+++ script.js
@@ -0,0 +1,19 @@
+"use strict";
+/*
+console.log(document.querySelector(".message").textContent);
+document.querySelector(".message").textContent = "Correct Number";
+
+console.log(document.querySelector(".message").textContent);
+
+document.querySelector(".number").textContent = 13;
+document.querySelector(".score").textContent = 17;
+
+document.querySelector(".guess").value = Math.trunc(Math.random() * 20);
+console.log(document.querySelector(".guess").value);
+*/
+
+document.querySelector(".check").addEventListener("click", function () {
+  console.log(document.querySelector(".guess").value);
+});
+
+console.log("hello people");

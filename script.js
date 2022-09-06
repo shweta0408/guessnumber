b8344b1ee138c9e2120063f16f57e1e274e11901
Shweta Mishra <shmis2004@gmail.com>
2022-09-06 16:43:02 +0530

    condition on secret numbeer

--- script.js
+++ script.js
@@ -12,18 +12,21 @@ document.querySelector(".guess").value = Math.trunc(Math.random() * 20);
 console.log(document.querySelector(".guess").value);
 */
 
-const number = Math.trunc(Math.random() * 21);
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector(".number").textContent = secretNumber;
 
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
 
   console.log(guess, typeof guess);
 
-  if (guess === number) {
-    document.querySelector(".message").textContent = "Correct";
-  } else {
-    console.log(
-      (document.querySelector(".message").textContent = "Not Correct")
-    );
+  if (!guess) {
+    document.querySelector(".message").textContent = "No Number";
+  } else if (guess === secretNumber) {
+    console.log((document.querySelector(".message").textContent = " Correct"));
+  } else if (guess > secretNumber) {
+    document.querySelector(".message").textContent = "Too high";
+  } else if (guess < secretNumber) {
+    document.querySelector(".message").textContent = "Too low";
   }
 });

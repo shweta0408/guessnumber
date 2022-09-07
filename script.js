a03d54c04dfe5d30ae745c137d46937923fbb23e
Shweta Mishra <shmis2004@gmail.com>
2022-09-07 13:35:15 +0530

    implemented again btn function

--- script.js
+++ script.js
@@ -12,15 +12,26 @@ document.querySelector(".guess").value = Math.trunc(Math.random() * 20);
 console.log(document.querySelector(".guess").value);
 */
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
-document.querySelector(".number").textContent = secretNumber;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 
 let score = 20;
-
+let highscore;
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
-
   console.log(guess, typeof guess);
+
+  document.querySelector(".again").addEventListener("click", function () {
+    score = 20;
+    secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+    document.querySelector(".message").textContent = "Start guessing...";
+    document.querySelector(".score").textContent = score;
+    document.querySelector(".number").textContent = "?";
+    document.querySelector(".guess").value = "";
+    document.querySelector("body").style.backgroundColor = "#222";
+    document.querySelector(".number").style.width = "15rem";
+    document.querySelector(".highscore").textContent = highscore;
+  });
 
   //when there is no input
   if (!guess) {
@@ -28,8 +39,9 @@ document.querySelector(".check").addEventListener("click", function () {
   }
   //when player wins
   else if (guess === secretNumber) {
-    console.log((document.querySelector(".message").textContent = " Correct"));
-
+    document.querySelector(".message").textContent = " Correct";
+    document.querySelector(".number").textContent = secretNumber;
+    highscore = document.querySelector(".score").textContent;
     document.querySelector("body").style.backgroundColor = "#60b347";
     document.querySelector(".number").style.width = "30rem";
 
